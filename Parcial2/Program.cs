15336d6e57bd8c986d294cf6b42e1dfb8421f2de
Enrriqu3 <125326292+Enrriqu3@users.noreply.github.com>
2023-04-18 20:40:14 -0600

    Parcial2

---
+++ Parcial2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more informatioint dato1 = 16;
+
+try
+{
+    int dato1 = 16;
+    string dato2 = "25g";
+    int Resultado = dato1 + Convert.ToInt32(dato2);
+}
+catch {
+    Console.WriteLine("Lo sentimos no se puede realizar la opcion solicitada");
+}
